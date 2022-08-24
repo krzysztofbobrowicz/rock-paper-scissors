9c2d81269f8b6c6c929d2219f1554d9069b25970
Krzysztof Bobrowicz <kbobrowicz98@gmail.com>
2022-08-24 17:20:53 +0200

    Create a game function that simulates the game and displays results in console

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let userScore = 0;
+let computerScore = 0;
 const rockPaperScissors = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3);
+  let randomNumber = Math.floor(Math.random() * 3);
   return rockPaperScissors[randomNumber];
 }
 
@@ -9,22 +11,44 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
     return "It's a draw!";
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    computerScore++;
     return 'You lose! Paper beats Rock.';
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    computerScore++;
     return 'You lose! Scissors cut Paper.';
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    computerScore++;
     return 'You lose! Rock destroys Scissors.';
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    userScore++;
     return 'You win! Rock destroys Scissors.';
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    userScore++;
     return 'You win! Paper beats Rock.';
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    userScore++;
     return 'You win! Scissors cut Paper.';
   } else {
     return 'something not ok.';
   }
 }
 
-const playerSelection = 'rock'.toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = getComputerChoice();
+    let userPrompt = prompt('What is your choice?');
+    console.log(playRound(userPrompt, computerSelection));
+  }
+  console.log('thats it folks!');
+  console.log(`user score is ${userScore}`);
+  console.log(`computer score is ${computerScore}`);
+  if (userScore > computerScore) {
+    console.log('The winner is user!');
+  } else if (userScore < computerScore) {
+    console.log('The winner is computer!');
+  } else {
+    console.log("That's a draw!");
+  }
+}
+
+game();
