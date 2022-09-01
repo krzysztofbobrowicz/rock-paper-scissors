b566510a9cf233ae8789909d533c5e22ebe48a91
Krzysztof Bobrowicz <kbobrowicz98@gmail.com>
2022-09-01 10:40:29 +0200

    Show changes in the DOM

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
 let userScore = 0;
 let computerScore = 0;
 const rockPaperScissors = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll('.btn');
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   return rockPaperScissors[randomNumber];
 }
-
+const container = document.querySelector('#container');
+const div = document.createElement('div');
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
     computerScore++;
     userScore++;
-    return "It's a draw!";
+    return (div.innerHTML = `It's a draw! User Score = ${userScore}. Computer Score = ${computerScore}`);
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     computerScore++;
-    return 'You lose! Paper beats Rock.';
+    return (div.innerHTML = `You lose! Paper beats Rock. User Score = ${userScore}. Computer Score = ${computerScore}`);
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     computerScore++;
-    return 'You lose! Scissors cut Paper.';
+    return (div.innerHTML = `You lose! Scissors cut Paper. User Score = ${userScore}. Computer Score = ${computerScore}`);
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     computerScore++;
-    return 'You lose! Rock destroys Scissors.';
+    return (div.innerHTML = `You lose! Rock destroys Scissors. User Score = ${userScore}. Computer Score = ${computerScore}`);
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     userScore++;
-    return 'You win! Rock destroys Scissors.';
+    return (div.innerHTML = `You win! Rock destroys Scissors. User Score = ${userScore}. Computer Score = ${computerScore}`);
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     userScore++;
-    return 'You win! Paper beats Rock.';
+    return (div.innerHTML = `You win! Paper beats Rock. User Score = ${userScore}. Computer Score = ${computerScore}`);
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     userScore++;
-    return 'You win! Scissors cut Paper.';
+    return (div.innerHTML = `You win! Scissors cut Paper. User Score = ${userScore}. Computer Score = ${computerScore}`);
   } else {
-    return 'something not ok.';
+    return (div.innerHTML =
+      'something not ok. User Score = ${userScore}. Computer Score = ${computerScore}');
   }
 }
 
 function game() {
-  while (userScore < 5 && computerScore < 5) {
-    console.log(userScore);
-    console.log(computerScore);
-    const computerSelection = getComputerChoice();
-    let userPrompt = prompt('What is your choice?');
-    console.log(playRound(userPrompt, computerSelection));
-  }
-  console.log('thats it folks!');
-  console.log(`user score is ${userScore}`);
-  console.log(`computer score is ${computerScore}`);
+  buttons.forEach(button => {
+    button.addEventListener('click', e => {
+      let userSelection = e.target.id;
+      const computerSelection = getComputerChoice();
+      playRound(userSelection, computerSelection);
+
+      container.appendChild(div);
+    });
+  });
+
   if (userScore > computerScore) {
     console.log('The winner is user!');
   } else if (userScore < computerScore) {
